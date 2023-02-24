cbbc619d7570f92c049abb81d9168dd13a09fe1b
Bristow Richards <57153542+bristowrichards@users.noreply.github.com>
2023-02-23 22:45:42 -0700

    add ratings download
    the setup script now handles the downloaded ratings data. a nice feature would be to do this programmatically instead of my manual download.

--- setup/make_data.R
+++ setup/make_data.R
@@ -1,5 +1,7 @@
 library(httr)
 library(sf)
+library(dplyr)
+library(readr)
 
 # countries layer ----
 
@@ -30,3 +32,55 @@ countries_sf <- st_read(request)
 
 ## save object ----
 saveRDS(countries_sf, 'src/data/countries_sf')
+
+# ratings data ----
+# same as before but this time not excluding 0 or blank elo
+# read data from download
+# todo(bristow): change to download on run isntead of link
+ratings <- read_fwf(
+  'C:/Users/brist/Downloads/players_list/players_list_foa.txt',
+  col_types = 'icccccccnnnnnnnnnnf',
+  skip=1
+)
+
+# copy columns
+# needed to do this to speed up column type, which required skipping line
+# readr::read_fwf doesn't allow to specify first row as colnames
+columns = c("ID Number", "Name", "Fed", "Sex", "Tit", "WTit", "OTit", 
+            "FOA", "SRtng", "SGm", "SK", "RRtng", "RGm", "Rk", "BRtng", 
+            "BGm", "BK", "Byear", "Flag")
+
+# set column names
+colnames(ratings) <- columns
+
+# subset to columns of interest
+ratings <- ratings[,c(1:6,9,12,15,18)]
+
+# country codes
+# todo(bristow) fix some of the `Fed` column to match iso3c
+
+# clean
+ratings <- ratings |>
+  # filter(
+  #   SRtng > 0, # must have rating and rating must be above 0
+  #   Byear > 1900 # must have valid birth year
+  # ) |>
+  mutate(
+    Bdecade = factor(sapply(Byear, function(x) x - x %% 10)),
+    Age = 2023 - Byear
+  )
+
+# save data
+saveRDS(ratings, file = 'src/data/ratings.Rds')
+
+# make list of all distinct countries
+# I was struggling to make this in the server side but still pass it 
+# to the ui side, so I'm just saving an .Rds object. Also: it's nice 
+# to use base R for this sometimes. I always default to tidyverse 
+# select |> filter |> summarize but table() works just fine.
+ratings$Fed |>
+  table() |> 
+  names() |> 
+  saveRDS(file = 'src/data/federations.Rds')
+
+# note to self: 30921406 is my FIDE number, but they got my number wrong!
