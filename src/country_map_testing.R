1f4676ec435a8fa5daf7054e09cc027947cf4739
Bristow Richards <57153542+bristowrichards@users.noreply.github.com>
2023-02-23 22:15:24 -0700

    Update country_map_testing.R
    this now successfully highlights a selected country based on some dummy input data

--- src/country_map_testing.R
+++ src/country_map_testing.R
@@ -1,6 +1,8 @@
 # countries
 library(leaflet)
+library(httr)
 library(sf)
+library(dplyr)
 
 # handle rest api for geojson output of world countries service from esri
 url <- parse_url('https://services.arcgis.com/P3ePLMYs2RVChkJx/arcgis/rest/services')
@@ -13,13 +15,33 @@ request <- build_url(url)
 # get data
 countries <- st_read(request)
 
+# dummy selection data
+test_country_data <- data.frame(
+  countries = c('US', 'IN', 'RU'),
+  selected = as.factor('selected')
+)
+
+# add dummy selection data
+countries <- countries |> 
+  left_join(
+    test_country_data,
+    by = c('ISO' = 'countries')
+  )
+
+# create custom palette
+my_pal <- colorFactor(
+  palette = 'RdYlBu',
+  domain = countries$selected,
+  na.color = NA
+)
+
 # simple viz
 leaflet(countries) |> 
-  addProviderTiles(providers$CartoDB.Positron) |> 
+  # addProviderTiles(providers$CartoDB.Positron) |> 
   addPolygons(
-    fillColor = 'red',
+    fillColor = ~my_pal(selected),
     stroke = TRUE,
-    fillOpacity = 0.2,
+    fillOpacity = 0.7,
     color = 'white',
     weight = 0.5,
     label = 'test_label'
